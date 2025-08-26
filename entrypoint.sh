4e7eb771bd079441381538f6530ed261bbb3503a
erina <erina.okabe55@gmail.com>
2025-08-26 14:26:02 +0900

    first commit

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# entrypoint.sh
+
+set -e
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /app/tmp/pids/server.pid
+
+# データベースが存在するかチェックし、なければ作成
+bundle exec rails db:prepare
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
